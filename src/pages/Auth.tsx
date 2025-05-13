62d74f59dd10b6e8a1e0d096034a25ae7098bd52
mfg5169 <mylesgould2024@u.northwestern.edu>
2025-05-13 00:07:38 -0500

    starting create

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -2,6 +2,7 @@ import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import reactLogo from '../assets/react.svg'; // Adjust the path if necessary
+import { DatePickerWithRange } from './CreateMeet2';
 
 export const Auth = () => {
   const clientId = import.meta.env.VITE_CLIENT_ID;
@@ -32,7 +33,10 @@ export const Auth = () => {
             />
           </div>
         ) : (
-          <img src={reactLogo} alt="React Logo" style={{ marginTop: "20px" }} />
+          <div>
+            <h1>Welcome to the App!</h1>
+            <DatePickerWithRange />
+          </div>
         )}
       </div>
     </GoogleOAuthProvider>
